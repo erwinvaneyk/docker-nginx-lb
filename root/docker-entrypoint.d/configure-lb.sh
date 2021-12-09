b7001b39273fcebbabe509d8dc6ff4e6a71abd00
erwinvaneyk <erwinvaneyk@gmail.com>
2021-12-09 11:48:58 +0100

    Restructure entrypoint script

--- root/docker-entrypoint.d/configure-lb.sh
+++ root/docker-entrypoint.d/configure-lb.sh
@@ -7,14 +7,10 @@ set -o pipefail
 NGINX_CONF=/etc/nginx/nginx.conf
 
 main() {
-  # TODO support args
   LB_PORT=${LB_PORT:-80}
   LB_BACKENDS=${LB_BACKENDS:-example.com:80}
 
-  complete_nginx_conf
-}
-
-complete_nginx_conf() {
+  # Complete the nginx.conf template
   UPSTREAM_BACKENDS=""
   for i in $(echo $LB_BACKENDS | sed 's|;| |g') ; do
     UPSTREAM_BACKENDS="$UPSTREAM_BACKENDS   server $$i;\n"
@@ -23,6 +19,7 @@ complete_nginx_conf() {
   sed -i "s|__LB_PORT__|${LB_PORT}|" "${NGINX_CONF}"
   sed -i "s|__UPSTREAM_BACKENDS__|${UPSTREAM_BACKENDS}|" "${NGINX_CONF}"
 
+  # Print the used nginx.conf for debugging
   cat "${NGINX_CONF}"
 }
 
