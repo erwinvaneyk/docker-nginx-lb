e6168898ce5037b2d972c285fba1ed0b70d7d350
jasmingacic <jasmin.gacic@gmail.com>
2022-02-20 20:44:06 +0000

    attempt to accept multiple 'upstream' servers in nginx.conf
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- root/docker-entrypoint.d/configure-lb.sh
+++ root/docker-entrypoint.d/configure-lb.sh
@@ -2,20 +2,21 @@
 
 set -o nounset
 set -o errexit
-set -o pipefail
+# set -o pipefail
 
-NGINX_CONF=/etc/nginx/nginx.conf
+# NGINX_CONF=/etc/nginx/nginx.conf
 
 main() {
   LB_PORT=${LB_PORT:-80}
-  LB_BACKENDS=${LB_BACKENDS:-example.com:80}
+  lb_backends=$(echo  ${LB_BACKENDS} | tr "," "\n") #split comma delimited string
 
   # Complete the nginx.conf template
   UPSTREAM_BACKENDS=""
-  for i in $(echo $LB_BACKENDS | sed 's|;| |g') ; do
-    UPSTREAM_BACKENDS="$UPSTREAM_BACKENDS   server $$i;\n"
+  for i in $(echo $lb_backends | sed 's|;| |g') ; do
+    UPSTREAM_BACKENDS="$UPSTREAM_BACKENDS   server $i;\n"
   done
 
+  # echo $UPSTREAM_BACKENDS
   sed -i "s|__LB_PORT__|${LB_PORT}|" "${NGINX_CONF}"
   sed -i "s|__UPSTREAM_BACKENDS__|${UPSTREAM_BACKENDS}|" "${NGINX_CONF}"
 
